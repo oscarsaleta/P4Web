80c6b0c2a468d6af73cd21ca1e974d4580b626ec
Oscar Saleta Reig <oscar.saleta@gmail.com>
2017-02-20 09:23:12 +0100

    try to solve cookies problem

--- src/main.cc
+++ src/main.cc
@@ -58,7 +58,7 @@ int main (int argc, char **argv)
 
     try {
         WServer server(argc, argv, WTHTTP_CONFIGURATION);
-        server.addEntryPoint(Wt::Application,createApplication,std::string(),"favicon.ico");
+        server.addEntryPoint(Wt::Application,createApplication,"/WP4.wt","favicon.ico");
         Session::configureAuth();
         server.run();
 
