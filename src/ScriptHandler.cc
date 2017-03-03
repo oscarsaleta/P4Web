752d2d145d991cdc08c763c7a9597802235f2147
Oscar Saleta Reig <oscar.saleta@gmail.com>
2017-03-03 12:56:13 +0100

    test commit with vsc

--- src/ScriptHandler.cc
+++ src/ScriptHandler.cc
@@ -1,5 +1,13 @@
-#include <vector>
+#include "MyLogger.h"
+
 #include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <vector>
 
 std::string ScriptHandler::openTempStream(std::string prefix, std::string suffix)
 {
@@ -15,7 +23,109 @@ std::string ScriptHandler::openTempStream(std::string prefix, std::string suffix
         close(fd);
     }
 
-    globalLogger__.debug("HomeLeft :: created temp file "+fullname);
+    globalLogger__.debug("ScriptHandler :: created temp file "+fullname);
 
     return prefix;
+}
+
+std::ofstream prepareMapleFile(std::string fname, mapleParamsStruct &prms)
+{
+    std::ofstream mplFile;
+
+    if (fname.empty())
+        fname = openTempStream(TMP_DIR,".mpl");
+
+    mplFile.open((fname+".mpl").c_str(),std::fstream::trunc);
+    
+    // repair file permissions
+    std::string command = "chmod 644 "+fname+".mpl";
+    system(command.c_str());
+
+    prms.str_vectable = fname+"_vec.tab";
+    prms.str_fintab = fname+"_fin.tab";
+    prms.str_finres = fname+"_fin.res";
+    prms.str_inftab = fname+"_inf.tab";
+    prms.str_infres = fname+"_inf.res";
+
+    return mplFile;
+}
+
+void HomeLeft::fillMapleScript(std::ofstream &f, mapleParamsStruct prms)
+{
+    f << "restart;" << std::endl;
+    f << "read( \"" << prms.str_p4m << "\" );" << std::endl;
+    f << "user_bindir := \"" << prms.str_bindir << "\":" << std::endl;
+    f << "user_tmpdir := \"" << prms.str_tmpdir << "\":" << std::endl;
+    f << "user_lypexe := \"" << prms.str_lypexe << "\":" << std::endl;
+    f << "user_sepexe := \"" << prms.str_sepexe << "\":" << std::endl;
+    f << "user_exeprefix := \"" << prms.str_exeprefix << "\":" << std::endl;
+    f << "user_platform := \"" << prms.str_platform << "\":" << std::endl;
+    f << "user_sumtablepath := \"" << prms.str_sumtablepath << "\":" << std::endl;
+    f << "user_removecmd := \"" << prms.str_removecmd << "\":" << std::endl;
+    f << "user_simplify := " << prms.str_simplify << ":" << std::endl;
+    f << "user_simplifycmd := " << prms.str_simplifycmd << ":" << std::endl;
+    f << "all_crit_points := " << prms.str_critpoints << ":" << std::endl;
+    f << "save_all := " << prms.str_saveall << ":" << std::endl;
+    f << "vec_table := \"" << prms.str_vectable << "\":" << std::endl;
+    f << "finite_table := \"" << prms.str_fintab << "\":" << std::endl;
+    f << "finite_res := \"" << prms.str_finres << "\":" << std::endl;
+    f << "infinite_table := \"" << prms.str_inftab << "\":" << std::endl;
+    f << "infinite_res := \"" << prms.str_infres << "\":" << std::endl;
+    f << "if (type(parse(\"" << xEquationInput_->text() << "\"),polynom)) then" << std::endl;
+    f << "  if (type(parse(\"" << yEquationInput_->text() << "\"),polynom)) then" << std::endl;
+    f << "    user_f := " << prms.str_userf << ":" << std::endl;
+    f << "  else `quit(1)` end if:" << std::endl;
+    f << "else `quit(1)` end if:" << std::endl;
+    f << "user_gcf := " << prms.str_gcf << ":" << std::endl;
+    f << "user_numeric :=" << prms.str_numeric << ":" << std::endl;
+    f << "epsilon := " << prms.str_epsilon << ":" << std::endl;
+    f << "test_sep := " << prms.str_testsep << ":" << std::endl;
+    f << "user_precision := " << prms.str_precision << ":" << std::endl;
+    f << "user_precision0 := " << prms.str_precision0 << ":" << std::endl;
+    f << "taylor_level := " << prms.str_taylor << ":" << std::endl;
+    f << "numeric_level := " << prms.str_numericlevel << ":" << std::endl;
+    f << "max_level := " << prms.str_maxlevel << ":" << std::endl;
+    f << "weakness_level := " << prms.str_weaklevel << ":" << std::endl;
+    f << "user_p := " << prms.str_userp << ":" << std::endl;
+    f << "user_q := " << prms.str_userq << ":" << std::endl;
+    f << "try timelimit("<<prms.time_limit<<",p4main()) catch:"  << std::endl
+        << "printf( \"! Error (\%a) \%a\\n\", lastexception[1], lastexception[2] );\n"
+        << "finally: closeallfiles();\n"
+        << "if normalexit=0 then `quit`(0); else `quit(1)` end if: end try:\n";
+}
+
+int evaluateMapleScript (std::string fname)
+{
+    pid_t pid = fork();
+    if (pid < 0) {
+        globalLogger__.error("HomeLeft :: error forking Maple thread.");
+        return pid;
+    } else if (pid == 0) {
+        // create vector of arguments for execvp
+        std::vector<char *> commands;
+#ifdef ANTZ
+        commands.push_back("ssh");
+        commands.push_back("p4@a01");
+        commands.push_back("-i");
+        commands.push_back("/var/www/claus_ssh/idrsa-1");
+#endif
+        commands.push_back(MAPLE_PATH);
+        commands.push_back("-T ,1048576"); // 1GB memory limit
+        char *aux = new char [fname.length()+1];
+        std::strcpy(aux,fname.c_str());
+        std::strcat(aux,".mpl");
+        commands.push_back(aux);
+        commands.push_back(nullptr);
+        // output from this thread goes to "fname.res"
+        int fd = open((fname+".res").c_str(),O_WRONLY|O_CREAT|O_TRUNC,0666);
+        dup2(fd,1);
+        // execute command
+        char **command = commands.data();
+        int status = execvp(command[0],command);
+        return status; //NOTE needed?
+    } else {
+        int status;
+        waitpid(pid,&status,0);
+        return status;
+    }
 }
--- src/ScriptHandler.h
+++ src/ScriptHandler.h
@@ -1,37 +1,59 @@
 #include "file_tab.h"
 
 #ifdef ANTZ
-    #define MAPLE_PATH "/usr/share/maple11/bin/maple"
-    #define P4_BINDIR "/home/p4/p4/bin/"
-    #define TMP_DIR "/home/p4/tmp/"
+#define MAPLE_PATH "/usr/share/maple11/bin/maple"
+#define P4_BINDIR "/home/p4/p4/bin/"
+#define TMP_DIR "/home/p4/tmp/"
 #else
-    #define MAPLE_PATH "/home/osr/maple2015/bin/maple"
-    #define P4_BINDIR "/usr/local/p4/bin/"
-    #define TMP_DIR "/tmp/"
+#define MAPLE_PATH "/home/osr/maple2015/bin/maple"
+#define P4_BINDIR "/usr/local/p4/bin/"
+#define TMP_DIR "/tmp/"
 #endif
 
-class ScriptHandler
-{
-public:
-    ScriptHandler(); //TODO ???
-    ~ScriptHandler();
-    
-    void setBaseFileName(std::string fname)
-    {
-        baseFileName_ = fname;
-    }
+struct mapleParamsStruct {
+    std::string str_bindir;
+    std::string str_p4m;
+    std::string str_tmpdir;
+    std::string str_lypexe;
+    std::string str_sepexe;
+    std::string str_exeprefix;
+    std::string str_platform;
+    std::string str_sumtablepath;
+    std::string str_removecmd;
+    std::string str_simplify;
+    std::string str_simplifycmd;
+    std::string str_critpoints;
+    std::string str_saveall;
+    std::string str_vectable;
+    std::string str_fintab;
+    std::string str_finres;
+    std::string str_inftab;
+    std::string str_infres;
+    std::string str_xeq;
+    std::string str_yeq;
+    std::string str_userf;
+    std::string str_gcf;
+    std::string str_numeric;
+    std::string str_epsilon;
+    std::string str_testsep;
+    std::string str_precision;
+    std::string str_precision0;
+    std::string str_taylor;
+    std::string str_numericlevel;
+    std::string str_maxlevel;
+    std::string str_weaklevel;
+    std::string str_userp;
+    std::string str_userq;
+    std::string time_limit;
+}
 
-    std::string openTempStream(std::string prefix, std::string suffix);
-    void prepareMapleFile(); // TODO: maybe return something
-    void fillMapleScript(std::ofstream& f); //TODO: com passar totes les variables? struct?
-    void evaluateMapleScript(); //NOTE: això ha de funcionar amb qualsevol maple script.
-    void prepareSaveFile(); //TODO: mirar inputs i outputs necessaris
+std::string openTempStream(std::string prefix, std::string suffix);
+std::ofstream prepareMapleFile(std::string fname, mapleParamsStruct& prms);
+void fillMapleScript(std::ofstream& f, mapleParamsStruct prms);
+void evaluateMapleScript(std::string fname);
+void fillSaveFile(mapleParamsStruct prms); //TODO: mirar inputs i outputs necessaris
 
-    // TODO copiar codi de file_vf.cpp i adaptar
-    bool prepareGcf( P4POLYNOM2 f, double, double, int, int );
-    bool prepareGcf_LyapunovCyl( double, double, int, int );
-    bool prepareGcf_LyapunovR2( int precision, int numpoints);
-
-private:
-    std::string baseFileName_; //TODO: treure això i usar fname com arguemnt de totes les funcions?
-};
+//TODO copiar codi de file_vf.cpp i adaptar
+bool prepareGcf( P4POLYNOM2 f, double, double, int, int );
+bool prepareGcf_LyapunovCyl( double, double, int, int );
+bool prepareGcf_LyapunovR2( int precision, int numpoints);
